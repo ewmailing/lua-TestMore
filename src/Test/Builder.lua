a9de3586f995fae0ef81fe71753e404718959700
Francois Perrad <francois.perrad@gadz.org>
2009-09-14 19:44:46 +0200

    start the real implementation

--- src/Test/Builder.lua
+++ src/Test/Builder.lua
@@ -4,71 +4,173 @@
 --
 
 local _G = _G
+local io = io
+local os = os
+local error = error
+local ipairs = ipairs
 local print = print
+local tostring = tostring
+local type = type
 
 module 'Test.Builder'
 
-local testnum = 1
-local failed = 0
-local planned = 0
-local started = 0
+local curr_test = 0
+local expected_tests = 0
 local todo_upto = 0
 local todo_reason
+local have_plan = false
 
-local function proclaim (cond, desc)
-    local msg = ''
-    if not cond then
-        msg = "not "
-        if todo_upto < testnum then
-            failed = failed + 1
+local out_file = io and io.stdout
+local fail_file = io and (io.stderr or io.stdout)
+local todo_file = io and io.stdout
+
+local function _print (...)
+    local f = output()
+    if f then
+        f:write(..., "\n")
+    else
+        print(...)
+    end
+end
+
+local function print_comment (f, ...)
+    if f then
+        local msg = ''
+        for _, v in ipairs{...} do
+            msg = msg .. tostring(v)
         end
+        msg = msg:gsub("\n", "\n# ")
+        f:write("# ", msg, "\n")
+    else
+        print("# ", ...)
     end
-    msg = msg .. "ok " .. testnum .. " - " .. desc
-    testnum = testnum + 1
-    if todo_reason and todo_upto >= testnum then
-        msg = msg .. todo_reason
+end
+
+function reset ()
+    curr_test = 0
+    expected_tests = 0
+    todo_upto = 0
+    have_plan = false
+end
+
+function plan (arg)
+    if have_plan then
+        error("You tried to plan twice")
     end
-    print(msg)
+    if type(arg) == 'string' and arg == 'no_plan' then
+        have_plan = true
+        return true
+    elseif type(arg) ~= 'number' then
+        error("Need a number of tests")
+    elseif arg <= 0 then
+        error("Number of tests must be a positive integer.  You gave it '" .. arg .."'")
+    else
+        expected_tests = arg
+        have_plan = true
+        _print("1.." .. arg)
+        return arg
+    end
 end
 
-function plan (num)
-        print("1.." .. num)
-        started = 1
-        testnum = 1
-        todo_upto = 0
-        failed = 0
-        planned = num
+function skip_all (reason)
+    if have_plan then
+        error("You tried to plan twice")
+    end
+    out = "1..0"
+    if reason then
+        out = out .. " # Skip " .. reason
+    end
+    _print(out)
+    os.exit(0)
 end
 
-function ok (cond, desc)
-    desc = desc or ''
-    proclaim(cond, desc)
+local function in_todo ()
+    return todo_upto >= curr_test
 end
 
-function diag (msg)
-    print(msg)
+function ok (test, name)
+    name = name or ''
+    if not have_plan then
+        error("You tried to run a test without a plan")
+    end
+    curr_test = curr_test + 1
+    name = tostring(name)
+    if name:match('^[%d%s]+$') then
+        diag("    You named your test '" .. name .."'.  You shouldn't use numbers for your test names."
+        .. "\n    Very confusing.")
+    end
+    local out = ''
+    if not test then
+        out = "not "
+    end
+    out = out .. "ok " .. curr_test .. " - " .. name
+    if todo_reason and in_todo() then
+        out = out .. " # TODO #" .. todo_reason
+    end
+    _print(out)
 end
 
-function note (msg)
-    print(msg)
-end
-
-function explain (msg)
-    print(msg)
+function BAIL_OUT (reason)
+    local out = "Bail out!"
+    if reason then
+        out = out .. "  " .. reason
+    end
+    _print(out)
+    os.exit(255)
 end
 
 function todo (reason, count)
     count = count or 1
-    todo_upto = testnum + count
-    todo_reason = " # TODO #" .. reason
+    todo_upto = curr_test + count
+    todo_reason = reason
 end
 
 function skip (reason, count)
-    reason = reason or ''
     count = count or 1
     for i = 1, count do
-        fail("# SKIP #" .. reason)
+        local out = "ok " .. curr_test .. " # skip"
+        if reason then
+            out = out .. " " .. reason
+        end
+        _print(out)
     end
+end
+
+local function diag_file ()
+    if in_todo() then
+        return todo_output()
+    else
+        return failure_output()
+    end
+end
+
+function diag (...)
+    print_comment(diag_file(), ...)
+end
+
+function note (...)
+    print_comment(output(), ...)
+end
+
+function output (f)
+    if f then
+        out_file = f
+    end
+    return out_file
+end
+
+function failure_output (f)
+    if f then
+        fail_file = f
+    end
+    return fail_file
+end
+
+function todo_output (f)
+    if f then
+        todo_file = f
+    end
+    return todo_file
 end
 
 --
