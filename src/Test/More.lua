26551e458196c2cd6ffd82be568d6d5ca9ce6e8f
Francois Perrad <francois.perrad@gadz.org>
2010-11-25 22:59:55 +0100

    prepare release 0.2.3

--- src/Test/More.lua
+++ src/Test/More.lua
@@ -362,7 +362,7 @@ end
 _G.Test = _G.Test or {}
 _G.Test.More = m
 
-m._VERSION = "0.2.2"
+m._VERSION = "0.2.3"
 m._DESCRIPTION = "lua-TestMore : an Unit Testing Framework"
 m._COPYRIGHT = "Copyright (c) 2009-2010 Francois Perrad"
 return m
